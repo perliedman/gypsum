d038d1752cd5e0ea5b510659fc750bc346be41c2
Per Liedman <per@liedman.net>
2010-08-22 08:24:43 +0200

    Sort of usable admin

--- positioning/admin.py
+++ positioning/admin.py
@@ -1,6 +1,14 @@
 from gypsum.positioning.models import Track, Activity
 from django.contrib import admin
+from datetime import timedelta
 
-admin.site.register(Track)
+def format_time(track):
+    return str(timedelta(seconds = track.time))
+
+class TrackAdmin(admin.ModelAdmin):
+    date_hierarchy = 'date'
+    list_display = ('date', 'owner', 'activity', 'distance', format_time)
+
+admin.site.register(Track, TrackAdmin)
 admin.site.register(Activity)
 
